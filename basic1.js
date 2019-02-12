3008e3edebc6022f87d17f7b039f692aad419245
Anesto27 <anes.2503@gmx.at>
2019-02-12 11:39:12 +0100

    create comments in basic1

--- basic1.js
+++ basic1.js
@@ -1,7 +1,7 @@
 var list = [1, 2, 3, 4, 5, 6, 7, 8, 10];
-//1.Step create a array called list with the numbers from 1 -10
-//2.Step use a for loop in a for loop and create two variables i and j and goes along the array length. 
-//3.Step we use document write to output the result while using a chain of strings and for loop variables
+//Step 1 create a array called list from 1-10
+//Step 2 using a for loop in a foor loop set two diffrent variables i and j and the loop goes through the whole array length until it stop.
+//Step 3 output the result solution and the result in a document write using strings and + to connect a chain to display the whole multiplication with the result.
 for (var i = 0; i < list.length; i++) {
     for (var j = 0; j < list.length; j++) {
         document.write(list[i] + "*" + list[j] + "=" + (list[i] * list[j]) + "<br>");
--- basic1.ts
+++ basic1.ts
@@ -1,5 +1,7 @@
 let list:number[] =[1,2,3,4,5,6,7,8,10];
-
+//Step 1 create a array called list from 1-10
+//Step 2 using a for loop in a foor loop set two diffrent variables i and j and the loop goes through the whole array length until it stop.
+//Step 3 output the result solution and the result in a document write using strings and + to connect a chain to display the whole multiplication with the result.
 for(var i=0;i<list.length;i++){
 	for(var j=0;j<list.length;j++){
 		document.write(list[i]+"*"+list[j]+"="+(list[i]*list[j])+"<br>");
