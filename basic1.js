e0d2c68d73d62073e6254c18f3c2e21f81a38598
Anesto27 <anes.2503@gmx.at>
2019-02-12 10:34:17 +0100

    finish basic exercise

--- basic1.js
+++ basic1.js
@@ -1,1 +1,9 @@
-var a = 12;
+var list = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+//1.Step create a array called list with the numbers from 1 -10
+//2.Step use a for loop in a for loop and create two variables i and j and goes along the array length. 
+//3.Step we use document write to output the result while using a chain of strings and for loop variables
+for (var i = 0; i < list.length; i++) {
+    for (var j = 0; j < list.length; j++) {
+        document.write(list[i] + "*" + list[j] + "=" + (list[i] * list[j]) + "<br>");
+    }
+}
--- basic1.ts
+++ basic1.ts
@@ -1,1 +1,10 @@
-var a : number = 12;
+let list:number[] =[1,2,3,4,5,6,7,8,10];
+
+for(var i=0;i<list.length;i++){
+	for(var j=0;j<list.length;j++){
+		document.write(list[i]+"*"+list[j]+"="+(list[i]*list[j])+"<br>");
+	}
+	
+	
+	
+}
